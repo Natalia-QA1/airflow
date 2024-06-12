9f41c535e12b9036ce5dd0294ecb2d3a8613fe2d
Natalia Ananeva <ananeva.natalia92@gmail.com>
2024-06-12 20:53:59 +0200

    imports optimozed

--- 5_email_operator.py
+++ 5_email_operator.py
@@ -1,5 +1,5 @@
-from airflow.models import DAG
 import pendulum
+from airflow.models import DAG
 from airflow.operators.email import EmailOperator
 from airflow.operators.empty import EmptyOperator
 
