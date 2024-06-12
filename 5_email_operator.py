5cc6284509d8852f6590218ae30270a9d926098d
Natalia Ananeva <ananeva.natalia92@gmail.com>
2024-06-12 20:38:27 +0200

    added task 5

---
+++ 5_email_operator.py
@@ -0,0 +1,27 @@
+from airflow.models import DAG
+import pendulum
+from airflow.operators.email import EmailOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG (
+    dag_id="email_sender",
+    start_date=pendulum.today(),
+    description="test DAG for email message sender",
+    catchup=False,
+
+) as dag:
+    start_op = EmptyOperator(
+        task_id="start"
+    )
+
+    send_email_op = EmailOperator(
+        task_id='send_email',
+        to='ananiev4nat@yandex.ru',
+        subject='Airflow Alert',
+        html_content='<p>Job has finished.</p>',
+        conn_id='smtp_default'
+    )
+
+    finish_op = EmptyOperator(
+        task_id="finish"
+    )
